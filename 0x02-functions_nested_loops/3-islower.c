5d1cadce0fc503a50b5c06b51b044f32c1f1cd22
Hailemariam Habtualem <Hailemariamhabtualem1@gmail.com>
2023-02-21 18:15:11 -0800

    finalised 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _islower :wq
- * _islower is a  lowercase character. Another cases, shows
+ *  _islower - Shows 1 if the input is a
+ * lowercase character. Another cases, shows
+ * 0
  * @c: The character in ASCII code
  *
  * Return: 1 for lowercase character. 0 for the rest.
